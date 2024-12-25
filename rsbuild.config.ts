6bf99fcec8253ec7f15f612b0f3e9203536ec479
Eden <huangsongsong@thinkingdata.cn>
2024-12-25 15:01:16 +0800

    chore: add build file

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from '@rsbuild/core';
 import { pluginReact } from '@rsbuild/plugin-react';
 import { pluginLess } from '@rsbuild/plugin-less';
+import path from 'path';
 
 export default defineConfig({
   plugins: [pluginLess(), pluginReact()],
   html: {
     title: 'drag move demo',
+  },
+  output: {
+    distPath: {
+      root: 'docs'
+    },
   }
 });
